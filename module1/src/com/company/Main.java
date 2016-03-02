7aef7a3f09620cc670f2d45114bc518216cbd516
shtroodel <yurok.andrey@gmail.com>
2016-03-02 23:03:43 +0200

    chaneged Main

--- module1/src/com/company/Main.java
+++ module1/src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
     }
 }
